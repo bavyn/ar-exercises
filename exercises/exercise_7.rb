6fb71a89a8c9d7d692f957928ca85cc09b5dcdd7
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-29 20:20:38 -0700

    attempt to create store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+store = Store.new(name: store_name)
+
+store.save ? puts("Store saved") : (puts "Error with saving store:"; puts store.errors.full_messages)
