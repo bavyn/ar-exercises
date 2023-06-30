597b611212bdac5cc020681d007ff2bf3a0ae2ad
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-29 19:25:56 -0700

    loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+puts "Store count: #{Store.count}"
+
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts "Store count: #{Store.count}"
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "----- Men's stores info -----"
+@mens_stores.each do |store|
+  puts "#{store.name} has an annual revenue of #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+puts "----- Women's stores info -----"
+@womens_stores.each do |store|
+  puts "#{store.name} has an annual revenue of #{store.annual_revenue}"
+end
